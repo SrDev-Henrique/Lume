2a70662e2470c823e7251e35461ad2a27e1dc071
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-01-08 18:36:40 -0300

    Fix sign-in sign-up layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,12 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <main className='flex mid-h-screen w=full justify-between font-inter'>
-      {children}
       <div className='auth-asset'>
         <div >
           <Image src='/icons/auth-image.svg' alt='auth image' width={500} height={500} />
         </div>
       </div>
+      {children}
     </main>
   );
 }
